c2ce442265b6bcc4d370c9d84118abb45465cad1
SK-HARI-01 <skhariharan2005@gmail.com>
2025-05-27 06:32:09 +0530

    Add GA for react

--- src/main.jsx
+++ src/main.jsx
@@ -2,5 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './App.css';
 import Toppage from './Toppage.jsx';
-
+import React from 'react-ga4'
+ReactGA.initialize("G-0NHSLTFVYD")
+ReactGA.send("pageview");
 createRoot(document.getElementById('root')).render(<Toppage />);
